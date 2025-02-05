f93caa25e851e2fef14c372f1d2768371c13cdc1
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2025-02-05 20:49:25 +0000

    kauai/src/groups2.cpp: remove uses of MacWin() macro
    Default to the Windows behaviour.

--- kauai/src/groups2.cpp
+++ kauai/src/groups2.cpp
@@ -621,7 +621,7 @@ bool GSTB::_FTranslateGrst(short osk)
         {
             wchar chw;
             CopyPb(PvAddBv(pvSrc, bstOld), &chw, SIZEOF(wchar));
-            if (osk == MacWin(koskUniWin, koskUniMac))
+            if (osk == koskUniMac)
                 SwapBytesRgsw(&chw, 1);
             cch = (long)(ushort)chw;
         }
