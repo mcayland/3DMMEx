8c6dc2b317dfb2da455a885cc052def7112b3b9e
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2025-02-06 22:11:26 +0000

    kauai/src/keys.h: ensure key constants are defined for non-Windows and non-Mac platforms

--- kauai/src/keys.h
+++ kauai/src/keys.h
@@ -18,17 +18,36 @@
 
 #define vkNil 0
 
-#define kvkLeft MacWin(0x1C, VK_LEFT)
-#define kvkRight MacWin(0x1D, VK_RIGHT)
-#define kvkUp MacWin(0x1E, VK_UP)
-#define kvkDown MacWin(0x1F, VK_DOWN)
-#define kvkHome MacWin(0x01, VK_HOME)
-#define kvkEnd MacWin(0x04, VK_END)
-#define kvkPageUp MacWin(0x0B, VK_PRIOR)
-#define kvkPageDown MacWin(0x0C, VK_NEXT)
+#ifdef WIN
 
-#define kvkBack MacWin(0x08, VK_BACK)
-#define kvkDelete MacWin(0x7F, VK_DELETE)
-#define kvkReturn MacWin(0x0D, VK_RETURN)
+#define kvkLeft VK_LEFT
+#define kvkRight VK_RIGHT
+#define kvkUp VK_UP
+#define kvkDown VK_DOWN
+#define kvkHome VK_HOME
+#define kvkEnd VK_END
+#define kvkPageUp VK_PRIOR
+#define kvkPageDown VK_NEXT
+
+#define kvkBack VK_BACK
+#define kvkDelete VK_DELETE
+#define kvkReturn VK_RETURN
+
+#else
+
+#define kvkLeft 0x1C
+#define kvkRight 0x1D
+#define kvkUp 0x1E
+#define kvkDown 0x1F
+#define kvkHome 0x01
+#define kvkEnd 0x04
+#define kvkPageUp 0x0B
+#define kvkPageDown 0x0C
+
+#define kvkBack 0x08
+#define kvkDelete 0x7F
+#define kvkReturn 0x0D
+
+#endif
 
 #endif //! KEYS_H
