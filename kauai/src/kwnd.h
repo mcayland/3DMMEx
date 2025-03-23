5ac7d6b400e0acaac75f3cc8a0307815fb0d2034
Ben Stone <1490287+benstone@users.noreply.github.com>
2025-03-23 14:35:34 +1100

    Update KWND class to add support for SDL_Window objects

--- kauai/src/kwnd.h
+++ kauai/src/kwnd.h
@@ -9,6 +9,11 @@
 #ifndef KAUAI_KWND_H
 #define KAUAI_KWND_H
 
+#ifdef KAUAI_SDL
+#include <SDL.h>
+#include <SDL_syswm.h>
+#endif
+
 #define kwndNil nullptr
 
 // Window handle class
@@ -24,6 +29,31 @@ class KWND
     KWND(HWND hwnd) : _hwnd(hwnd)
     {
     }
+
+#ifdef KAUAI_SDL
+
+    // Create a KWND from an SDL Window handle
+    explicit KWND(SDL_Window *wnd)
+    {
+        _wnd = wnd;
+        _hwnd = HwndFromSDLWindow(_wnd);
+    }
+
+    // Get SDL Window handle
+    explicit operator SDL_Window *() const
+    {
+        return _wnd;
+    }
+
+    // Set the KWND to an SDL window handle
+    KWND &operator=(SDL_Window *wnd)
+    {
+        _wnd = wnd;
+        _hwnd = HwndFromSDLWindow(_wnd);
+        return *this;
+    }
+
+#endif // KAUAI_SDL
 
     // Get Win32 HWND
     operator HWND() const
@@ -55,6 +85,25 @@ class KWND
 
   private:
     HWND _hwnd = nullptr;
+
+#ifdef KAUAI_SDL
+    SDL_Window *_wnd = nullptr;
+
+    static HWND HwndFromSDLWindow(SDL_Window *wnd)
+    {
+        SDL_SysWMinfo wmInfo;
+        SDL_VERSION(&wmInfo.version);
+        if (SDL_GetWindowWMInfo(wnd, &wmInfo))
+        {
+            return wmInfo.info.win.window;
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+
+#endif // KAUAI_SDL
 };
 
 #endif // KAUAI_KWND_H
