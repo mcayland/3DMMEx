6b78060dbf1ef9bf565ac9ea0bf3523ee70ca791
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2025-05-09 22:34:59 +0100

    src/engine/srec.cpp: fix WAVHDR dwUser DWORD_PTR cast
    According to the API documentation the dwUser field is DWORD_PTR and not
    DWORD which is required to avoid pointer truncation on 64-bit Windows.

--- src/engine/srec.cpp
+++ src/engine/srec.cpp
@@ -132,7 +132,7 @@ bool SREC::_FOpenRecord(void)
         }
 
         // prepare header on block of data
-        _wavehdr.dwUser = (DWORD)this;
+        _wavehdr.dwUser = (DWORD_PTR)this;
         if (waveInPrepareHeader(_hwavein, &_wavehdr, sizeof(WAVEHDR)))
         {
             waveInClose(_hwavein);
