9ae7bfc9639aed016fe709f3f22ea17268c8dd44
Ben Stone <1490287+benstone@users.noreply.github.com>
2025-09-14 14:39:16 +1000

    Add Portfolio stub for non-Windows platforms

---
+++ src/studio/portfstub.cpp
@@ -0,0 +1,15 @@
+/***************************************************************************
+
+    portfstub.cpp: Portfolio stub
+
+***************************************************************************/
+#include "studio.h"
+
+ASSERTNAME
+
+bool FPortDisplayWithIds(FNI *pfni, bool fOpen, int32_t lFilterLabel, int32_t lFilterExt, int32_t lTitle,
+                         PCSZ lpstrDefExt, PSTN pstnDefFileName, FNI *pfniInitialDir, uint32_t grfPrevType, CNO cnoWave)
+{
+    Bug("Portfolio not implemented on this platform");
+    return fFalse;
+}
