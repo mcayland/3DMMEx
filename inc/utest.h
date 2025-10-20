2ce15ba7e0abbda87623f95abb967caa7e541553
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2025-10-20 21:43:13 +0100

    inc/utest.h: replace DWORD type with cstdint uint32_t type
    Replace the Windows-specific DWORD type with the cstdint standard uint32_t type.

--- inc/utest.h
+++ inc/utest.h
@@ -213,7 +213,7 @@ class APP : public APP_PAR
     bool _FInitBuilding(void);
     bool _FInitStudio(PFNI pfniUserDoc, bool fFailIfDocOpenFailed = fTrue);
     bool _FInitAcceleratorTable(void);
-    void _GetWindowProps(int32_t *pxp, int32_t *pyp, int32_t *pdxp, int32_t *pdyp, DWORD *pdwStyle);
+    void _GetWindowProps(int32_t *pxp, int32_t *pyp, int32_t *pdxp, int32_t *pdyp, uint32_t *pdwStyle);
     void _RebuildMainWindow(void);
     bool _FSwitch640480(bool fTo640480);
     bool _FDisplayIs640480(void);
