d78dceae41976badcf1f5b99016d58b1a405e075
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2025-11-22 22:20:30 +0000

    WIP: remove __cdecl

--- kauai/tools/chomp.cpp
+++ kauai/tools/chomp.cpp
@@ -14,11 +14,7 @@ ASSERTNAME
     Main routine for the stand-alone chunky compiler.  Returns non-zero
     iff there's an error.
 ***************************************************************************/
-#ifndef WIN
-#define __cdecl
-#endif
-
-int __cdecl main(int cpszs, char *prgpszs[])
+int main(int cpszs, char *prgpszs[])
 {
     FNI fniSrc, fniDst;
     PCFL pcfl;
