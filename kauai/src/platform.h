9ab10f132bfc840c28e131d6dd2066243614631b
Ben Stone <1490287+benstone@users.noreply.github.com>
2026-03-08 17:27:04 +1100

    Add Signal object to platform module

--- kauai/src/platform.h
+++ kauai/src/platform.h
@@ -6,6 +6,9 @@
 #define PLATFORM_H
 
 #include <stdint.h>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
 
 /****************************************
     Mutex (critical section) object
@@ -23,6 +26,54 @@ class MUTX
 
     void Enter(void);
     void Leave(void);
+};
+
+/****************************************
+    Signal - simulates a Win32 Event
+****************************************/
+class Signal
+{
+  public:
+    // Wake up a thread that is waiting for this event
+    void Set()
+    {
+        {
+            std::lock_guard<std::mutex> lock(_mutx);
+            _fSignaled = true;
+        }
+
+        _cv.notify_one();
+    }
+
+    // Wait for the event to be signalled
+    bool Wait(uint32_t dtsTime = 0xFFFFFFFF)
+    {
+        std::unique_lock<std::mutex> lock(_mutx);
+
+        if (dtsTime == 0xFFFFFFFF)
+        {
+            // Wait forever
+            _cv.wait(lock, [&] { return _fSignaled; });
+            _fSignaled = false;
+            return true;
+        }
+        else
+        {
+            // Wait for the given number of milliseconds
+            auto deadline = std::chrono::steady_clock::now() + std::chrono::milliseconds(dtsTime);
+            bool fResult = _cv.wait_until(lock, deadline, [&] { return _fSignaled; });
+            if (fResult)
+            {
+                _fSignaled = false;
+            }
+            return fResult;
+        }
+    }
+
+  private:
+    std::mutex _mutx;
+    std::condition_variable _cv;
+    bool _fSignaled = false;
 };
 
 /****************************************
